5778ed13846eab4d15fe572b98b444649125f133
pkopy <pawel.kopycki@gmail.com>
2018-11-22 13:13:32 +0100

    HW2 - preparing

---
+++ HW-2/config.js
@@ -0,0 +1,30 @@
+/*
+ * Create and export configuration variables
+ *
+ */
+
+//Container for all the environments
+const environments  = {};
+
+//Staging (default) environment
+environments.staging = {
+  'httpPort' : 3000,
+  'httpsPort' : 3001,
+  'envName' : 'staging'
+};
+
+//Production environment
+environments.production = {
+  'port' : 5000,
+  'httpsPort' : 5001,
+  'envName' : 'production'
+};
+
+//Determine which evironment was passed as a command-line argument
+const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+//Check that the current environment is one of the enviornments above, if not, default to staging
+const environmetToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+//Export the module
+module.exports = environmetToExport;
---
+++ HW-2/index.js
@@ -0,0 +1,118 @@
+/*
+ *
+ * Primary file for API
+ * 
+ */
+
+// Dependencies
+const http = require('http');
+const https = require('https')
+const url = require('url');
+const config = require('./config');
+const fs = require('fs');
+
+
+
+// Instantiate the HTTP Server
+const httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res);
+});
+
+//Start the HTTP server 
+httpServer.listen(config.httpPort, () => {
+  
+  console.log(`The server is listening on the port ${config.httpPort}`);
+});
+
+// Instantiate the HTTPS Server
+const httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert' : fs.readFileSync('./https/cert.pem')
+};
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  unifiedServer(req, res);
+});
+
+//Start the HTTPS Sserver 
+httpsServer.listen(config.httpsPort, () => {
+  console.log(`The server is listening on the port ${config.httpsPort}`);
+});
+
+//All the server logic for both the http and https server
+const unifiedServer = ((req, res) => {
+  //Get the url and parse it
+  const parsedUrl = url.parse(req.url, true)
+  
+  //Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  
+  //Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  //Get the method
+  const method = req.method.toLowerCase();
+
+  //Get the headers as an object
+  const headers = req.headers;
+
+  let payload = '';
+  req.on('data', data => {
+    //Get the payload, if any
+    payload = Buffer.from(data).toString();
+  });
+
+  req.on('end', () => {
+
+    //Choose the handler this request should go to, if one is not found choose the notFound handler
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    //Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload
+    };
+
+    //Route the request to handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      //Use the status code called back by the handler or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      //Use the payload called back by the handler or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      //Convert the payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      //Return the response
+      res.writeHead(statusCode,{
+        'Content-Type': 'application/json'
+      });
+      res.end(payloadString);
+
+      //Log the payload
+      console.log('Returning this response: ', statusCode, payloadString)
+    });
+  });
+});
+
+//Define the handlers
+const handlers = {};
+
+//Ping handler
+handlers.ping = ((data,callback) => {
+  callback(200);
+})
+
+//Not found handler 
+handlers.notFound = ((data, callback) => {
+  callback(404);
+})
+
+//Define a request router
+const router = {
+  'ping' : handlers.ping
+}
